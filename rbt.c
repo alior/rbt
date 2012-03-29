a911c358c684da9b0b42a036adff9e6d3fbe6111
unknown <alior@alior-PC.(none)>
2012-03-29 10:23:52 +0200

    cyk

--- rbt.c
+++ rbt.c
@@ -9,13 +9,14 @@ typedef struct W {
 	int kolor;
 } wezel;
 
-void LEFTROTATE(wezel *root, wezel *x){
+wezel* LEFTROTATE(wezel *root, wezel *x){
 	wezel *y=x->right;
 	x->right=x->left; //lewe poddrzewo na prawe
 	y->left->p=x;
 	y->p=x->p;  //ojcem y uczyn ojca x
 	if(x->p==NULL)
 		root=y;
+
 	else {
 		if (x==x->p->left)
 			x->p->left=y;
@@ -23,29 +24,30 @@ void LEFTROTATE(wezel *root, wezel *x){
 	}
 	y->left=x;
 	x->p=y;
-
+	return root;
 }
 
-void RIGHTROTATE(wezel *root, wezel *x){
+wezel* RIGHTROTATE(wezel *root, wezel *x){
 	wezel *y=x->left;
-		x->left=x->right; //lewe poddrzewo na prawe
-		y->right->p=x;
-		y->p=x->p;  //ojcem y uczyn ojca x
-		if(x->p==NULL)
-			root=y;
-		else {
-			if (x==x->p->right)
-				x->p->right=y;
-			else x->p->left=y;
-		}
-		y->right=x;
-		x->p=y;
+	x->left=x->right; //lewe poddrzewo na prawe
+	y->right->p=x;
+	y->p=x->p;  //ojcem y uczyn ojca x
+	if(x->p==NULL)
+		root=y;
+	else {
+		if (x==x->p->right)
+			x->p->right=y;
+		else x->p->left=y;
+	}
+	y->right=x;
+	x->p=y;
+	return root;
 }
 
 wezel* RBFIX(wezel *root, wezel *z){
 	wezel *y;
-	if(root!=z){
-	while(z->p->kolor==RED){
+
+	while(root!=z && z->p->kolor==RED){
 		if(z->p == z->p->p->left){
 			y = z->p->p->right;
 			if(y->kolor==RED){
@@ -57,11 +59,11 @@ wezel* RBFIX(wezel *root, wezel *z){
 			else{
 				if(z==z->p->right){
 				z=z->p;
-				LEFTROTATE(root,z);
+				root=LEFTROTATE(root,z);
 				}
 				z->p->kolor=BLACK;
 				z->p->p->kolor=RED;
-				RIGHTROTATE(root,z->p->p);
+				root=RIGHTROTATE(root,z->p->p);
 			}
 		}
 		else{
@@ -75,14 +77,13 @@ wezel* RBFIX(wezel *root, wezel *z){
 			else{
 				if(z==z->p->left){
 				z=z->p;
-				RIGHTROTATE(root, z);
+				root=RIGHTROTATE(root, z);
 				}
 				z->p->kolor=BLACK;
 				z->p->p->kolor=RED;
-				LEFTROTATE(root, z->p->p);
+				root=LEFTROTATE(root, z->p->p);
 			}
 		}
-	}
 	}
 	root->kolor = BLACK;
 	return root;
@@ -148,16 +149,19 @@ int main() {
 	int wartosc;
 	char znak;
 	wezel *root=NULL;
-	while(znak!='x'){
-		printf("Co chcesz zrobic? ");
+	printf("Co chcesz zrobic? ");
+	while(znak!='x') {
 		scanf("%c", &znak);
 		if(znak=='+'){
 			scanf("%d",&wartosc);
 			root = RBinsert(root,wartosc);
 		}
 		else if(znak=='p'){
+			printf("digraph G {\n");
 			wyswietl(root);
+			printf("}");
 		}
+//		printf("\n")
 	}
 	printf("Narazie..");
 	return 0;
